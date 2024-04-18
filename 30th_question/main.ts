3037fabbd2be4a3e5baf01be17dc8ff1f0bbc87f
Umair_Jami <lalaumair92@gmail.com>
2024-04-18 14:09:13 +0500

    Updated code

--- 30th_question/main.ts
+++ 30th_question/main.ts
@@ -3,13 +3,12 @@
 
 // • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
 
+let usernames: string[] = ["Admin", "Eric", "Nick", "Umair", "Jhon"];
 
-let usernames:string[]=['Admin','Eric','Nick','Umair','Jhon']
-
-usernames.forEach(username => {
-    if (username === "Admin") {
-        console.log("Hello admin, would you like to see a status report?");
-    } else {
-        console.log(`Hello ${username}, thank you for logging in again.`);
-    }
+usernames.forEach((username) => {
+  if (username === "Admin") {
+    console.log("Hello admin, would you like to see a status report?");
+  } else {
+    console.log(`Hello ${username}, thank you for logging in again.`);
+  }
 });
